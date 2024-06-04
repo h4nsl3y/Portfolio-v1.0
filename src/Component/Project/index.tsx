2cab80d7cc5246a61abaaf3263df075b5a637970
h4nsl3y <hansleyeleonore@gmail.com>
2024-06-04 23:30:34 +0400

    multi-tab project, implemented

--- src/Component/Project/index.tsx
+++ src/Component/Project/index.tsx
@@ -4,7 +4,7 @@ const index = () => {
   return (
     <div className="w-full h-full flex elementScreen" id='Project'>
         <div className="h-full w-full">
-            <div className="h-[90vh] sm:h-[90%] w-[95%] grid auto-rows-[90vh] sm:grid-cols-2 sm:grid-rows-2 grid-flow-row sm:grid-flow-col m-auto overflow-y-scroll sm:overflow-hidden">
+            <div className="h-[90vh] sm:h-[90%] w-[95%] grid auto-rows-[90vh] sm:auto-rows-[40vh] sm:grid-cols-2 grid-flow-row  m-auto overflow-y-scroll">
                 <div className={`row-span-1 col-span-1 ${boxStyle}`}>b1</div>
                 <div className={`row-span-1 col-span-1 ${boxStyle}`}>b2</div>
                 <div className={`row-span-1 col-span-1 ${boxStyle}`}>b3</div>
