084934a892dd8bc23514b71a599f5a2cb9710d0d
h4nsl3y <hansleyeleonore@gmail.com>
2024-06-07 19:29:04 +0400

    Addition of CV option

--- src/Component/Navbar/index.tsx
+++ src/Component/Navbar/index.tsx
@@ -1,16 +1,18 @@
 import NotificationType from "@/Types/NotificationType"
 import navigation_icon from '@/assets/image/navigation_icon.png';
 import home_icon from '@/assets/image/home_icon.png';
-import user_icon from '@/assets/image/user_icon.png'
-import code_icon from '@/assets/image/code_icon.png'
+import user_icon from '@/assets/image/user_icon.png';
+import code_icon from '@/assets/image/code_icon.png';
+import document_icon from '@/assets/image/document_icon.png';
 import phone_icon from '@/assets/image/phone_icon.png';
 import email_icon from '@/assets/image/email_icon.png';
 import linkdin_icon from '@/assets/image/linkdin_icon.png';
 import github_icon from '@/assets/image/github_icon.png';
 
+import curriculumVitae from '@/assets/document/Curriculum Vitae.pdf';
 
-let buttonClass = 'w-[50px] sm:w-full aspect-square flex item-center justify-center rounded bg-transparent transition-colors duration-300 ease-in-out hover:bg-[#7b3aff80] hover:shadow-lg hover:shadow-black active:bg-[#7b3aff] ';
-let imageClass = 'w-2/5 h-2/5 m-auto dark:invert';
+let buttonClass = 'h-[50px] sm:h-fit w-full aspect-square flex flex-row rounded text-center items-center rounded bg-transparent transition-colors duration-300 ease-in-out hover:bg-[#7b3aff80] hover:shadow-lg hover:shadow-black active:bg-[#7b3aff] ';
+let imageClass = 'w-[20px] h-[20px] sm:w-2/5 sm:h-2/5 m-[10px] sm:m-auto dark:invert';
 
 type Props = {
   option: string;
@@ -39,16 +41,16 @@ type Props = {
     let notificationData: NotificationType = {title:"title", contentType:"text", content:"content" };
     switch(id){
       case "phone":
-        notificationData = {title:"Phone : ", contentType:"text", content:"+230-5807-8946" };
+        notificationData = {title:"Phone : ", contentType:"text", content:"+230-5807-8946"};
         break;
       case "email":
-        notificationData = {title:"Email : ", contentType:"email", content:"hansleyeleonore@gmail.com" };
+        notificationData = {title:"Email : ", contentType:"email", content:"hansleyeleonore@gmail.com"};
         break;
       case "linkdin":
-        notificationData = {title:"Linkdin : ", contentType:"href", content:"https://www.linkedin.com/in/hansley-eleonore-b17705181" };
+        notificationData = {title:"Linkdin : ", contentType:"href", content:"https://www.linkedin.com/in/hansley-eleonore-b17705181"};
         break;
       case "github":
-        notificationData = {title:"Github : ", contentType:"href", content:"https://github.com/h4nsl3y" };
+        notificationData = {title:"Github : ", contentType:"href", content:"https://github.com/h4nsl3y"};
         break;
     }
     setNotification(notificationData);
@@ -59,28 +61,50 @@ type Props = {
       navbar.style.height =  navbar.style.height == "400px" ? "50px" : "400px";
     }
   }
+  const downloadCV = () =>{
+
+  }
 
   return (
     <div className={`h-[50px] w-full sm:h-full sm:w-[5%] sm:max-w-[100px] flex flex-col justify-between rounded shadow-sm sm:shadow-lg shadow-black sm:shadow-black overflow-hidden transition-transform ease-in-out duration-300`} id='navbar'>
+      
+      {window.innerWidth <= 640 &&
       <div className='h-fit w-fit sm:w-full flex item-center justify-center'>
         <button className={`${buttonClass} bg-[#5050a8] dark:bg-[#121231]`}  onClick={()=>expand()}>
           <img src={navigation_icon} className={`${imageClass}`}/>
         </button>
       </div>
+       }
 
         <div className='w-full h-fit flex flex-col item-center justify-center'>
-          <label className={`${buttonClass} ${option=='Home' ? 'bg-[#7b3aff80]' : '' }`}>
-            <input type="radio" name="screen" value="Home" className={`absolute opacity-0 w-[0px] h-[0px]`} onChange={handleOptionChange}/>
-            <img src={home_icon} className={`${imageClass} `}/>
-          </label>
-          <label className={`${buttonClass} ${option=='About' ? 'bg-[#7b3aff80]' : '' }`}>
-          <input type="radio" name="screen" value="About" className={`absolute opacity-0 w-[0px] h-[0px]`} onChange={handleOptionChange}/>
-            <img src={user_icon} className={`${imageClass}`}/>
-          </label>
-          <label className={`${buttonClass} ${option=='Project' ? 'bg-[#7b3aff80]' : '' }`}>
-          <input type="radio" name="screen" value="Project" className={`absolute opacity-0 w-[0px] h-[0px]`} onChange={handleOptionChange}/>
-            <img src={code_icon} className={`${imageClass}`}/>
-          </label>
+        <div className={`w-full h-fit flex flex-row rounded text-center items-center `} >
+            <label className={`${buttonClass}`}>
+              <input type="radio" name="screen" value="Home" className={`absolute opacity-0 w-[0px] h-[0px]`} onChange={handleOptionChange}/>
+              <img src={home_icon} className={`${imageClass}`}/>
+              {window.innerWidth <= 640 && <p>Home</p> }
+            </label>
+          </div>
+          <div className={`w-full h-fit flex flex-row rounded text-center items-center`}>
+            <label className={`${buttonClass}`}>
+              <input type="radio" name="screen" value="About" className={`absolute opacity-0 w-[0px] h-[0px]`} onChange={handleOptionChange}/>
+              <img src={user_icon} className={`${imageClass}`}/>
+              {window.innerWidth <= 640 && <p>About</p> }
+            </label>
+          </div>
+          <div className={`w-full h-fit flex flex-row rounded text-center items-center`}>
+            <label className={`${buttonClass}`}>
+              <input type="radio" name="screen" value="Project" className={`absolute opacity-0 w-[0px] h-[0px]`} onChange={handleOptionChange}/>
+              <img src={code_icon} className={`${imageClass}`}/>
+              {window.innerWidth <= 640 && <p>Project</p> }
+            </label>
+          </div>
+          <div className={`w-full h-fit flex flex-row rounded text-center items-center`}>
+            <a href={curriculumVitae} download='Curriculum Vitae.pdf' className={`${buttonClass}`}>
+              <input type="radio" name="screen" value="Project" className={`absolute opacity-0 w-[0px] h-[0px]`} onClick={ () => downloadCV()}/>
+              <img src={document_icon} className={`${imageClass}`}/>
+              {window.innerWidth <= 640 && <p>Curriculum Vitae</p> }
+            </a>
+          </div>
         </div>
 
         <div className='w-full h-fit flex flex-row sm:flex-col item-center justify-between sm:justify-center'>
