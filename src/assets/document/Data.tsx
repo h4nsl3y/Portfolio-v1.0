4b6ab195d084a9958528f00525dba5512c40179b
h4nsl3y <hansleyeleonore@gmail.com>
2025-03-23 11:46:41 +0400

    Updating linkdin link

--- src/assets/document/Data.tsx
+++ src/assets/document/Data.tsx
@@ -88,7 +88,7 @@ export default {
     "Notification":{
         "phone": {"Title" : "Phone : ", "ContentType" : "text", "Content" : "+230-5807-8946"},
         "email" : {"Title" : "Email : ", "ContentType" : "email", "Content" : "hansleyeleonore@gmail.com"},
-        "linkdin" : {"Title" : "Linkdin : ", "ContentType" : "href", "Content" : "https://www.linkedin.com/in/hansley-eleonore-b17705181"},
+        "linkdin" : {"Title" : "Linkdin : ", "ContentType" : "href", "Content" : "www.linkedin.com/in/hansley-eleonore"},
         "github" : {"Title" : "Github : ", "ContentType" : "href", "Content" : "https://github.com/h4nsl3y"},
         "none" : {"Title" : "", "ContentType" : "", "Content" : ""}
     },
